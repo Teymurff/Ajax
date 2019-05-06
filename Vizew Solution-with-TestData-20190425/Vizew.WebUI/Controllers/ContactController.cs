62fba0070f61d6180eaf0d7e20f43eaf3030bfc2
Teymur <you@example.com>
2019-05-06 23:30:56 +0400

    Hello

---
+++ Vizew Solution-with-TestData-20190425/Vizew.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vizew.WebUI.Models;
+using Vizew.WebUI.Models.Entity;
+
+namespace Vizew.WebUI.Controllers
+{
+ 
+
+    public class ContactController : Controller
+    {
+        VizewDbContext db = new VizewDbContext();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string Name,string Email,string Message)
+        {
+            Contact contact = new Contact();
+            contact.Name = Name;
+            contact.Email = Email;
+            contact.Message = Message;
+            db.Contact.Add(contact);
+            db.SaveChanges();
+        
+
+            return Json(contact, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ Vizew Solution-with-TestData-20190425/Vizew.WebUI/Models/Entity/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vizew.WebUI.Models.Entity
+{
+    public class Contact:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
